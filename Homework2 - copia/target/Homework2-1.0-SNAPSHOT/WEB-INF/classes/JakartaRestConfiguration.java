f511326bfe692c6ce716858ba2e341631051ddf0
wanna-throw <pdarioalexander@gmail.com>
2026-01-08 15:21:22 +0100

    a little bit of this, a little bit of that

--- Homework2 - copia/target/Homework2-1.0-SNAPSHOT/WEB-INF/classes/JakartaRestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package deim.urv.cat.homework2;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/mvc")
-public class JakartaRestConfiguration extends Application {
- 
-}
--- Homework2 - copia/target/classes/exception/PropertyException.java
+++
@@ -1,8 +0,0 @@
-package deim.urv.cat.homework2.exception;
-
-public class PropertyException extends RuntimeException {
-
-    public PropertyException( String message){
-        super(message);
-    }
-}
